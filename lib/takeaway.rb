87ded46beb7c723677f93580af96e37d9f91d14d
stefaniacardenas <stefaniaferullo@hotmail.com>
2014-04-25 14:11:07 +0100

    Created a menu with prices

---
+++ lib/takeaway.rb
@@ -0,0 +1,17 @@
+class Takeaway
+
+	def menu
+		menu = { 
+			"Pizza Buffalina" => 15 ,
+			"Mozzarella Sticks" => 5,
+			"Pizza Patate" => 12,
+			"Pizza Funghi" => 12,
+			"Pizza Olive" => 12,
+			"Tiramisu" => 5, 
+			"Cheese cake" => 5,
+			"Limoncello" => 8,
+			"Negroni" => 4,
+			"Spritz" => 4
+		}
+	end
+end
